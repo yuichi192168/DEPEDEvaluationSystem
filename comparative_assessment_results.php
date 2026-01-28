1cbba26ca9220044f19e8114eac5eefcbb9d67f8
sheweliz <antinerosheweliz02@gmail.com>
2026-01-28 14:47:01 +0800

    Add favicon links to comparative_assessment_results.php

--- comparative_assessment_results.php
+++ comparative_assessment_results.php
@@ -72,6 +72,12 @@ if ($viewMode === 'all') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Comparative Assessment Result (CAR) - DepEd HRMPSB</title>
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="manifest" href="images/site.webmanifest">
     <style>
         * {
             margin: 0;
